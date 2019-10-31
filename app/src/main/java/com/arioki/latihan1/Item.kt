514de460c3cb5ea46c492a75a5ab9ca2013df206
yoga setiawan <yogainformatika@gmail.com>
2019-10-31 16:38:22 +0700

    first commit

---
+++ app/src/main/java/com/arioki/latihan1/Item.kt
@@ -0,0 +1,3 @@
+package com.arioki.latihan1
+
+data class Item(val name:String?, val image:Int?)
---
+++ app/src/main/java/com/arioki/latihan1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.arioki.latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private var items : MutableList<Item> = mutableListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initData()
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this,items)
+    }
+
+    private fun initData() {
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+
+        items.clear()
+        for (i in name.indices){
+            items.add(Item(name[i],image.getResourceId(i, 0)))
+        }
+        image.recycle()
+    }
+
+}
